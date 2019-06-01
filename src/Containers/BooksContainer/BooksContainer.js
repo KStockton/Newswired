5fb1d583f1d62e7edb1d8107eada5e0ccfab0e6e
Michael KS <kingstockton@gmail.com>
2019-05-31 18:22:13 -0600

    add grid for books

--- src/Containers/BooksContainer/BooksContainer.js
+++ src/Containers/BooksContainer/BooksContainer.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
 const BooksContainer = ({books}) => {
-  
+    console.log(books)
+  const bookGrid = books.map(book => {
+    const { bookImage, rank, author, description, title, weeksOnList} = book
+    return (
+      <article className="book-tile">
+        <p>Rank: {rank}</p>
+        <img className="book-img" src={bookImage} alt="book"/>
+        <p className="author">{author}</p>
+        <p>{title}</p>
+      </article>
+    )
+  })
+
   return (
-    <article className=''>
-      
-    </article>
+    <div className='book-wrapper'>
+      {bookGrid}
+    </div>
   )
 }
 
--- src/Utility/Cleaners/cleanBooks.js
+++ src/Utility/Cleaners/cleanBooks.js
@@ -1,7 +1,7 @@
 export const cleanBooks = (uncleanBooks) => {
  const cleanedBooks = uncleanBooks.results.books.map(book => {
     return {
-        id: Date.now(),
+        id: book.author,
         rank: book.rank,
         author: book.contributor,
         bookImage: book.book_image,
