eaf61a4c23eb12bf6508ce282e4aad094bb71554
Michael KS <kingstockton@gmail.com>
2019-05-31 19:31:30 -0600

    fix book scss draft

--- src/Containers/BooksContainer/BooksContainer.js
+++ src/Containers/BooksContainer/BooksContainer.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from 'react';
+const shortid = require('shortid')
 
 const BooksContainer = ({books}) => {
     console.log(books)
   const bookGrid = books.map(book => {
     const { bookImage, rank, author, description, title, weeksOnList} = book
     return (
-      <article className="book-tile">
+      <article key={shortid.generate()} className="book-tile">
         <p>Rank: {rank}</p>
         <img className="book-img" src={bookImage} alt="book"/>
         <p className="author">{author}</p>
-        <p>{title}</p>
+        
       </article>
     )
   })
