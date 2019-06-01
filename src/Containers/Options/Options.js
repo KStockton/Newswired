99ceca54ad771f87fe2214c587d14dd5f5e61e0c
Michael KS <kingstockton@gmail.com>
2019-05-31 20:29:27 -0600

    style splash page to build functionality

---
+++ src/Containers/Options/Options.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookOpen, faFire, faFilm, faRss } from '@fortawesome/free-solid-svg-icons';
+
+
+// import PropTypes from 'prop-types'
+
+export class Options extends Component {
+  constructor() {
+    super()
+    this.state = {
+      numOfSelect: 0
+    }
+  }
+
+  componentDidMount() {
+  //   this.getDate()
+  // }
+  // static propTypes = {
+  //   prop: PropTypes
+  }
+  increment = () => {
+
+    this.setState({numOfSelect: this.state.numOfSelect + 1 })
+  }
+
+  render() {
+   
+    
+
+    return (
+      <div>
+        <div className="Options-header">
+          <h1 className="Options-title">News Wire</h1>
+        </div>
+      <header>
+        <p className="Options-select">Select Your Favorites</p>
+      </header>
+
+      <div className="Options-wrapper">
+
+        <label htmlFor="top-news">
+          <FontAwesomeIcon id="top-news" className="Options-fontawesome" onClick={this.increment} icon={faFire}/>
+          Top News
+        </label>
+
+        <label htmlFor="new-york-times-books">
+          <FontAwesomeIcon id="new-york-times-books" className="Options-fontawesome" onClick={this.increment} icon={faBookOpen}/>
+            New York Time Books
+        </label>
+
+        <label htmlFor="movie-review">
+          <FontAwesomeIcon id="movie-review" className="Options-fontawesome" onClick={this.increment} icon={faFilm}/>
+            Movie Review
+        </label>
+
+        <label htmlFor="latest-news">
+          <FontAwesomeIcon id="latest-news" className="Options-fontawesome" onClick={this.increment} icon={faRss}/>
+            Latest News
+        </label>
+
+        </div>
+      </div>
+    )
+  }
+}
+
+
+
+export default Options
