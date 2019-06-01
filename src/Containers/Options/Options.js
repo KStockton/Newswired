cd0ff64319a5f9f7bef7a8bd4bbec03eb9067c36
Michael KS <kingstockton@gmail.com>
2019-05-31 20:59:00 -0600

    finish splash style

--- src/Containers/Options/Options.js
+++ src/Containers/Options/Options.js
@@ -39,25 +39,33 @@ export class Options extends Component {
 
       <div className="Options-wrapper">
 
+      <button className="Options-btn">
         <label htmlFor="top-news">
           <FontAwesomeIcon id="top-news" className="Options-fontawesome" onClick={this.increment} icon={faFire}/>
           Top News
         </label>
+        </button>
 
+<button className="Options-btn">
         <label htmlFor="new-york-times-books">
           <FontAwesomeIcon id="new-york-times-books" className="Options-fontawesome" onClick={this.increment} icon={faBookOpen}/>
             New York Time Books
         </label>
+</button>
 
+<button className="Options-btn">
         <label htmlFor="movie-review">
           <FontAwesomeIcon id="movie-review" className="Options-fontawesome" onClick={this.increment} icon={faFilm}/>
             Movie Review
         </label>
+        </button>
 
+        <button className="Options-btn">
         <label htmlFor="latest-news">
           <FontAwesomeIcon id="latest-news" className="Options-fontawesome" onClick={this.increment} icon={faRss}/>
             Latest News
         </label>
+        </button>
 
         </div>
       </div>
