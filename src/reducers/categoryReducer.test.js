ec3c4d3371ee4e4e23c311fbf775a33b277674f5
Michael KS <kingstockton@gmail.com>
2019-06-03 22:56:54 -0600

    add categoryReducer test

---
+++ src/reducers/categoryReducer.test.js
@@ -0,0 +1,18 @@
+import { categoryReducer } from './categoryReducer';
+
+describe('categoryReducer', () =>{
+  it('should return a default state if the action has no type', () => {
+    const mockState = {topNews: false, books: false, sportsNews: false, travel: false}
+    
+    const mockAction = ''
+    const results = categoryReducer(mockState,mockAction)
+    expect(results).toEqual(mockState)
+  })
+  it('should return a new state if a category is passed',() => {
+    const mockState = {topNews: false, books: false, sportsNews: false, travel: false}
+    const expected = {topNews: true, books: false, sportsNews: false, travel: false}
+    const mockaction = {type: "SET_NEWS_CATEGORY", category: "topNews"}
+    const results = categoryReducer(mockState, mockaction)
+    expect(results).toEqual(expected)
+  })
+})
