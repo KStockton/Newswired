c33fb46eb9a7793180fb3c223c9920d4276b256c
Michael KS <kingstockton@gmail.com>
2019-06-05 09:59:15 -0600

    fix single item

--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -5,16 +5,20 @@ import {Link} from 'react-router-dom'
 const Item = ({bookImage, rank, author, id, description, title, weeksOnList}) => {
 
   return (
+    <div>
+      <Link to={`/card/${id}`} className="Item-back">
+      </Link>
       <article className="Item-wrapper" id={id}>
-          <Link to={`/card/${id}`}>
-            <img className="Item-img" src={bookImage} alt="book"/>
-              </Link>
-            <p className="Item-author">{author}</p>
-            <p className="Item-rank">Rank: {rank}</p>
-            <p className="Item-weeks">{weeksOnList} weeks on list</p>
-            <p className="Item-title">{title}</p>
-            <p className="Item-description">{description}</p>
-          </article >
+        <section className="Item-content-wrapper">
+          <img className="Item-img" src={bookImage} alt="book"/>
+          <p className="Item-author">{author}</p>
+          <p className="Item-rank">Rank: {rank}</p>
+          <p className="Item-weeks">{weeksOnList} weeks on list</p>
+          <p className="Item-title">{title}</p>
+          <p className="Item-description">{description}</p>
+        </section>
+      </article >
+    </div>
   )
 } 
 export default Item
