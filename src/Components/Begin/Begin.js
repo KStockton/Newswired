ef59add70ece6ce52173e38e3fa3fc970366e972
Michael KS <kingstockton@gmail.com>
2019-06-04 20:50:16 -0600

    fix begin page

--- src/Components/Begin/Begin.js
+++ src/Components/Begin/Begin.js
@@ -11,7 +11,7 @@ const Begin = () => {
           <span id="news-wire">to News Wire</span>
         </h1>  
       </section>
-      <Link to="/Options">
+      <Link to="/Options" className="start">
         <button className="begin-btn">Get Started</button>
       </Link>
     </div>
