94b4109ea7e4baa748d65f9727974b0513a78797
Michael KS <kingstockton@gmail.com>
2019-08-03 11:15:08 -0600

    Fix sports url fetch with more recent day

--- src/Containers/SportContainer/SportContainer.js
+++ src/Containers/SportContainer/SportContainer.js
@@ -11,13 +11,28 @@ const shortid = require('shortid')
 export class SportContainer extends Component {
 
  componentDidMount = () => {
-    
+
+  const todaysDate = this.date()
+
     if(this.props.allSports.length === 0){
-      const sportNewsUrl = `https://newsapi.org/v2/everything?q=sports&language=en&from=2019-06-03&page=1&domains=espn.com&apiKey=${process.env.REACT_APP_NEWSAPI_API_KEY}`;
+      const sportNewsUrl = `https://newsapi.org/v2/everything?q=sports&language=en&from=${todaysDate}&page=1&domains=espn.com&apiKey=${process.env.REACT_APP_NEWSAPI_API_KEY}`;
       this.props.fetchSportsNews(sportNewsUrl)
     }
   }
   
+  date = () => {
+
+      var d = new Date(),
+          month = '' + (d.getMonth() + 1),
+          day = '' + d.getDate(),
+          year = d.getFullYear();
+  
+      if (month.length < 2) month = '0' + month;
+      if (day.length < 2) day = '0' + day;
+  
+      return [year, month, day - 1].join('-');
+  
+  }
 
   displaySportNews = () => {
    return this.props.allSports.map(location => {
